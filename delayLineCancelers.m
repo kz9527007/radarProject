b3ae49e11400b351e738174b225c7debc09f3436
swrangsar basumatary <swrangsar@example.com>
2014-03-22 12:02:59 +0530

    commit at 20140322120259

---
+++ delayLineCancelers.m
@@ -0,0 +1,18 @@
+f = linspace(0, 7, 1000);
+w = 2*pi*f;
+singleLC = 4 * (sin(w./2).^2);
+doubleLC = singleLC.^2;
+singleLC = sqrt(abs(singleLC));
+doubleLC = sqrt(abs(doubleLC));
+singleLC = singleLC ./ (max(singleLC));
+doubleLC = doubleLC ./ (max(doubleLC));
+singleLC = 10*log10(singleLC + eps);
+doubleLC = 10*log10(doubleLC + eps);
+
+figure('Name','Delay Line Cancellers');
+plot(f,singleLC, f, doubleLC);
+title('Delay Line Cancelers');
+% axis('tight');
+xlabel('normalized frequency, f/f_r');
+ylabel('amplitude response in dB');
+hleg = legend('single DLC', 'double DLC', 'Location', 'NorthEastOutside');
