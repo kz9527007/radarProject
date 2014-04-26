f3260e49fa7a14b546a6f523311fd1e824f999ae
swrangsar basumatary <swrangsar@example.com>
2014-04-26 15:54:04 +0530

    commit at 20140426155403

--- prfSimulation.m
+++ prfSimulation.m
@@ -11,30 +11,42 @@ xlabel('time in seconds');
 ylabel('amplitude');
 
 
-h1 = zeros(1024);
+h1 = zeros(1,1024);
 for k = (63+1):1000
     h1(k) = sig(k) - sig(k-63);
 end
 
-h2 = zeros(1024);
+h2 = zeros(1,1024);
 for k = (64+1):1000
     h2(k) = sig(k) - sig(k-64);
 end
 
 h = h1 + h2;
+g = zeros(1,1024);
+for k = 1:length(sig)
+    g(k) = sig(k);
+end
+
 fs = 1/.001
 H = abs(fft(h, 1024));
+G = abs(fft(g, 1024));
 len = length(H);
 w = (0:len-1)*fs;
 w = w./len;
 figure('Name', 'frequency spectrum');
 clf;
+subplot(2,1,1);
 plot(w,H);
 axis tight;
-title('Frequency spectrum');
+title('filtered frequency spectrum');
 xlabel('Frequency, Hz');
 ylabel('Amplitude');
-
+subplot(2,1,2);
+plot(w,G);
+axis tight;
+title('unfiltered frequency spectrum');
+xlabel('Frequency, Hz');
+ylabel('Amplitude');
 
 f1 = 1/(.001*63)
 fdip = f1*63.5
